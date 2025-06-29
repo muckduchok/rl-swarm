c41d0e0e980c706e43d57c57b657f2b9bbac6ddf
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-06-29 14:37:08 +0300

    Update run_rl_5.sh

--- run_rl_5.sh
+++ run_rl_5.sh
@@ -269,15 +269,16 @@ fi
 
 echo -en $GREEN_TEXT
 #read -p ">> Enter the name of the model you want to use in huggingface repo/name format, or press [Enter] to use the default model. " MODEL_NAME
+MODEL_NAME=""
 echo -en $RESET_TEXT
 
 # Only export MODEL_NAME if user provided a non-empty value
-# if [ -n "$MODEL_NAME" ]; then
-#     export MODEL_NAME
-#     echo_green ">> Using model: $MODEL_NAME"
-# else
-#     echo_green ">> Using default model from config"
-# fi
+if [ -n "$MODEL_NAME" ]; then
+    export MODEL_NAME
+    echo_green ">> Using model: $MODEL_NAME"
+else
+    echo_green ">> Using default model from config"
+fi
 
 echo_green ">> Good luck in the swarm!"
 echo_blue ">> And remember to star the repo on GitHub! --> https://github.com/gensyn-ai/rl-swarm"
