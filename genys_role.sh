3994abf1af5e4e4b4314089f53c1a7a30fbc412d
Anton Avhutskiy <41346887+muckduchok@users.noreply.github.com>
2025-08-09 17:39:43 +0300

    Update genys_role.sh

--- genys_role.sh
+++ genys_role.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# сделать go видимым для этого процесса
+export PATH=/usr/local/go/bin:$PATH
+
+if ! command -v make >/dev/null 2>&1; then
+  apt-get update && apt-get install -y make
+fi
+
 TOKEN_BOT=$(cat gensyn_bot/token_bot.txt)
 CHAT_ID=$(cat gensyn_bot/chat_id.txt)
 EVM=$(cat gensyn_bot/evm_address.txt)
@@ -16,6 +26,7 @@ gswarm --version
 git clone https://github.com/Deep-Commit/gswarm.git
 cd gswarm
 
+source ~/.bashrc
 /usr/bin/make build
 /usr/bin/make install
 
